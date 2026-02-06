440b2981599383fd5457c6c96fdc61c7e925de86
Rifqi Singgih <rifqisinggih@gmail.com>
2026-02-06 11:07:54 +0700

    mengubah judul web, contact section ke wa, logo footer ke ig dan email, menambahkan logo joro, dan merapihkan lagi css html

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// dibawah ini kode dari orang pintar saya blm paham
 // toogle class active
 
 const navbarNav = document.querySelector(".navbar-nav");
@@ -16,4 +17,32 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// gilakkk woi bingung
+// ini untuk aktifkan kirim pesa langsung ke wa gais, ada versi email tapi males mending wa awokwowk
+// ini alur nya ges = 1. User isi form 2. Klik Kirim 3. JavaScript ambil data 4. Buat link WhatsApp 5. Buka WhatsApp di tab baru 6. Form dikosongkan.
+const contactForm = document.querySelector("#contact-form"); //untuk "menangkap" elemen HTML yang memiliki ID #contact-form
+
+contactForm.addEventListener("submit", function (event) {
+  //Program akan "mendengarkan" atau menunggu sampai pengguna menekan tombol submit.
+  event.preventDefault(); //Fungsi ini mencegah refresh halaman agar js dulu yg mikir gais
+
+  //ini untuk mengambil value yg user ketik di kolom contact
+  const nama = document.getElementById("nama-user").value;
+  const mail = document.getElementById("email-user").value;
+  // const nomer = document.getElementById("nomer-user").value;
+
+  console.log(nama, mail); // untuk function active
+
+  // kita masuk ke ranah WA direct link dan pesan
+  // note : %0A untuk membuat baris baru (Enter)
+  const noWa = "628992247100";
+  const pesan =
+    `hello buddy my name is ${nama} %0A%0A` +
+    `My email ${mail} %0A` +
+    `I really intrested with your coffee journey`;
+
+  const url = `https://wa.me/${noWa}?text=${pesan}`;
+
+  window.open(url, "_blank"); //untuk aktif kan function di lembar baru
+
+  contactForm.reset(); //untuk mengosongkan kembali semua kolom input di form agar bersih seperti semula
+});
